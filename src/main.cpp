de97b730939c54dc2ecfe97ef8542a4b3652c14b
Kevin <kevin.grundhall01@gmail.com>
2026-02-10 20:18:30 +0100

    Updated Template, 10/02-26

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,9 @@
-#define RAYLIB
-#ifdef RAYLIB
-#include "raylib.h"
-#endif
-
 #include <iostream>
-#include <string>
 
-int main(int argc, char **argv)
+int main()
 {
-    std::cout << "Hello from cpp-template!\n";
 
-#ifdef RAYLIB
-    InitWindow(800, 450, "Template App");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Ready for flashcards!", 180, 200, 30, DARKGRAY);
-        EndDrawing();
-    }
-    CloseWindow();
-#else
-    std::cout << "(Console mode – add raylib by uncommenting Fetch + header)\n";
-#endif
+    std::cout << "Hello World!\n";
 
     return 0;
 }
